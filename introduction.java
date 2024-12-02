5a5514caf8f9799ef2b53ef518505f779e7e1e28
Shahyadburhan <shahyadburhan0@gmail.com>
2024-12-02 23:00:27 +0430

    Add files via upload

---
+++ introduction.java
@@ -0,0 +1,13 @@
+
+package javaapplication16;
+
+public class JavaApplication16 {
+
+    public static void main(String[] args) {
+        System.out.println(" I am Shahyad son of Burhanuddin."
+                + "\n I graduated from school in 2023 now I am 19 years old. I live in kabul city."
+                + " \n I am student of camputer science in kabul university."
+                + "\n And My favorite sports are football and table tennis.");
+    }
+    
+}
